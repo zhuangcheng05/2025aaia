df0a8ead61fee2e31f41b8383da7de71ac0e242d
zhuangcheng05 <zhuangcheng05@gmail.com>
2025-03-14 12:07:02 +0800

    Delete week03-1.cpp

--- week03-1.cpp
+++
@@ -1,9 +0,0 @@
-//week03-1.cpp 二合一的C++版本
-//Leetcode 2579.Count Total Number of Colored Cells
-class Solution {
-public:
-    long long coloredCells(int n) {
-        long long ans = n;//要多寫這行，把數字升級，便64位元{很長很長的整數}
-        return ans*ans+(ans-1)*(ans-1);//C/C++/Java/C#都須加分號
-    }
-};
